b1a94547813503dbb82f5b48604068cf9baafffc
Trinatek <trinatek@gmail.com>
2021-10-27 02:04:07 -0500

    Removed instructions multi-line comments.

--- simplepw.py
+++ simplepw.py
@@ -1,44 +1,6 @@
 import random
 import string
 
-"""
-SUMMARY:
-    Generates a user customized randomized password. Supports special characters,
-    digits, uppercase, lowercase. Passwords will have at least one of every specified
-    character type. The minimum password length is 4 characters.
-
-    Default settings for password options are:
-        - Password length:           10
-        - Special characters:     False
-        - Digits:                  True
-        - Uppercase:               True
-        - Lowercase:               True
-
-
-INSTRUCTIONS:
-
-    # EXAMPLE 1: Create and configure a SimplePW object, then generate.
-
-    SPW = SimplePW()  
-    SPW.length = 12  
-    SPW.special_chars = True  
-    SPW.uppercase = True  
-    SPW.lowercase = True  
-    password = SPW.get()  
-
-    # EXAMPLE 2: Create a password with default settings.
-    default_password = SimplePW().get()  
-
-    # EXAMPLE 3: Create a password with custom options in one line.  
-    custom_password = SimplePW(length=50, digits=True, uppercase=False, lowercase=False).get() 
-
-
-AUTHOR:
-    Napoleon Vang
-    trinatek@gmail.com
-
-"""
-
 
 class SimplePW:
     def __init__(
